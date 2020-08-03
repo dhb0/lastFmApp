93dc1bacedc86651269f7e9bfe80fbd9514f0d45
Deniz H BEYAZ <denizhbeyaz@gmail.com>
2020-08-03 17:21:01 +0300

    Prop drilling solved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import ArtistCardHolder from "./Components/ArtistCardHolder";
+import InputField from "./Components/InputField";
 
 function App() {
 
+const [query, setQuery] = useState('');
+
+  const buttonClicked = (query) =>{
+      setQuery(query)
+  }
+
   return (
     <div className="App">
-      <ArtistCardHolder  />
+      <InputField buttonClicked={buttonClicked} />
+      <ArtistCardHolder query={query} />
     </div>
   );
 }
--- src/Components/ArtistCardHolder.js
+++ src/Components/ArtistCardHolder.js
@@ -3,33 +3,40 @@ import ArtistCard from './ArtistCard';
 import InputField from './InputField';
 import axios from 'axios';
 
-function ArtistCardHolder() {
+function ArtistCardHolder(props) {
 
     const [data, setData] = useState([]);
+    const { query } = props;
 
     const fetchData = (query) => {
         axios
-          .get("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar",
-            {
-              params: {
-                artist: query,
-                limit: 10,
-                api_key: 'f9ad13943c8b617e4b903d0f5bbf2041',
-                format: 'json'
-              }
+            .get("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar",
+                {
+                    params: {
+                        artist: query,
+                        limit: 10,
+                        api_key: 'f9ad13943c8b617e4b903d0f5bbf2041',
+                        format: 'json'
+                    }
+                })
+            .then(response => {
+                setData(response.data.similarartists.artist);
             })
-          .then(response => {
-            setData(response.data.similarartists.artist);
-          })
-          .catch(error => console.log(error));
-      }
+            .catch(error => console.log(error));
+    }
+
+    useEffect(() => {
+        if (query !== '') {
+            fetchData(query);
+        }
+    }, [query])
 
     return (
         <>
-         <InputField buttonClicked={fetchData} />
-        <div className="datafield">
-            {data.map(item=><ArtistCard bandName={item.name} image={item['image'][1]["#text"]} match={parseFloat(item.match)} url={item.url} />)}
-        </div>
+
+            <div className="datafield">
+                {data.map(item => <ArtistCard bandName={item.name} image={item['image'][1]["#text"]} match={parseFloat(item.match)} url={item.url} />)}
+            </div>
         </>
     )
 }
